351694b3ed0c7d595a96e48565a238349bdd5e0c
vikrantChaudhary <vikrant.symantec@gmail.com>
2014-11-07 17:43:23 +0530

    Create AboutUs.php

---
+++ AboutUs.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head id="ctl00_Head1">
+<title>BigM:Building Manufacturing EcosysteM</title>
+
+<meta charset="UTF-8" />
+
+<meta name="description" content="Lean Consulting, Improvement, Operations, Business Process Management, Business Process Improvement, BPM, BPR, BPI." />
+
+<meta name="keywords" content="Lean Consulting, Improvement, Operations, Business Process Management, Business Process Improvement, BPM, BPR, BPI, 
+Business Process Reengineering, Implementation, Transformation, Enterprise Architecture, Operational Architecture, Operational Strategy, Business Strategy, 
+Outsourcing, Business Process Outsourcing, Diagnostics, Analytics, Efficiency, Lean 6 Sigma, Performance Management, Cost Reduction, Productivity, 
+Metric Management, Call Centre, Contact Centre, Administration, Document Management, Six Sigma, Operations Solutions" />
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link href="Style/StyleSheet.css" type="text/css" rel="stylesheet" />
+
+<link href="Style/style.css" type="text/css" rel="stylesheet" />
+
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" />
+
+<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+
+
+
+<script src="js/bootstrap.js" type="text/javascript"></script>
+
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+    <script src="js/jquery-1.3.2.min.js" type="text/javascript"></script>
+ 
+<script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-49043309-1', 'bigm.co.in.com');
+        ga('send', 'pageview');
+
+</script>
+    <style type="text/css">
+        .nav-tabs-justified {}
+        #Text1 {
+            margin-left: 12px;
+            }
+        .nav-tabs-justified {
+            margin-left: 60px;
+        }
+        .auto-style1 {
+            width: 394px;
+            height: 41px;
+            margin-right: 50px;
+        }
+        .block-menu { overflow:hidden; !important;}
+    </style>
+
+
+
+</head>
+
+
+<body>
+
+
+<div class="wrapper">
+<div class="header">
+   		
+	<hr style="position:absolute;left:180px;top:0px;width:1010px;height:190%;"/>
+	
+	<div class="logo" style="position:absolute;left:200px;top:10px;">
+		<a href="index.php"> <img src="images/logo.png" width="245" height="115" alt="Logo" /></a>
+	</div>
+	
+
+	<div class="header_right_section">
+        
+	     <ul class="menu" style="position:absolute;left:775px;top:10px;font-size:125%"
+		<li class="item1" ><a href="http://www.bigm.co.in/index.php"><span>Home</span></a></li>
+		<li class="item2" style="position:absolute;left:150px;top:0px;font-size:100%"><a href="http://www.bigm.co.in" target="_blank"><span>CaseStudies</span></a>
+		</li><li class="item3" style="position:absolute;left:300px;top:0px;font-size:100%"><a href="http://www.bigm.co.in/ContactUs.php"><span>ContactUs</span></a></li>
+		</ul>
+	     <ul class="menu" />	
+	
+	</div> <!-- Closing The header_right_section Div -->
+
+	 <hr style="position:absolute;left:185px;top:120px;width:1000px;height:310px;"/>
+
+
+        <div class="banner" style="position:absolute;left:190px;top:140px;border="10";">
+
+	<img src="images/Banner_Our_Approach.jpg" width="990" height="290" alt="Banner" style="position:relative;" />
+	</div>
+<!--
+
+	<div class="tagline"  style="position:absolute;left:450px;top:100px;font-size:150%" >Experience: Analyze:Transform</div>
+        </div> 
+-->
+        <div class="clear"></div>
+
+       	<script type="text/javascript" src="js/hoverIntent.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+ 
+
+<!-- Search Option Beign-->
+<!--
+<li>                         
+                   <input name="ctl00$q" type="text" id="ctl00_q" style="position:absolute;left:800px;top:100px;width:80px;"/>
+                         <span id="ctl00_RadioButtonList1" style="display:inline-block;color:White;font-family:Myriad Pro;font-size:Medium;height:13px;width:100px;margin-top: 0px; margin-bottom: 0px">
+		   <input id="ctl00_RadioButtonList1_0" type="radio" name="ctl00$RadioButtonList1" value="BigM" checked="checked" style="position:absolute;left:900px;top:100px;"/>
+			<label for="ctl00_RadioButtonList1_0">BigM</label>
+		   <input id="ctl00_RadioButtonList1_1" type="radio" name="ctl00$RadioButtonList1" value="Google" style="position:absolute;left:980px;top:100px;"/>
+			<label for="ctl00_RadioButtonList1_1">Google</label></span>
+                        &nbsp;&nbsp;&nbsp;
+		   <input type="submit" name="ctl00$Button1" value="Search" id="ctl00_Button1" style="position:absolute;left:1070px;top:100px;color:#56A850;background-color:White;font-family:Myriad Pro;font-size:Medium;font-weight:bold;height:25analyzae
+px;width:89px;margin-top: 0px; margin-bottom: 0px;/>
+             </li> 
+-->
+<!-- Search Option Ends-->
+
+<div >
+
+	<div class="navigation" >
+
+    	<ul class="dropdown">
+        
+ 	 <li  style="position:absolute;left:250px;top:450px"  ><a href="index.php" id="n1">Home</a>  
+           <ul style="display:none" class="sub_menu">
+                <li><a href="OurApproach.php">Our Approach</a></li>
+                <li><a href="OurServices.php">Our Services</a></li>
+                <li><a href="OurIndustries.php">Our Industries</a></li>
+                <li><a href="OurClients.php">Our Exposure</a></li>
+                <li><a href="ContactUs.php">Contact Us</a></li>
+           </ul>
+        </li>  
+
+
+	<li style="position:absolute;left:600px;top:450px"" ><a href="AboutUs.php" id="n3">About Us</a> 
+           <ul style="display:none;width:120px;left:-14px;" class="sub_menu">
+                <li><a href="OurTeam.php">Our  Team</a></li>
+                <li><a href="OurAssociates.php">Our  Associates</a></li>
+                <li><a href="OurVision.php">Our  Vision</a></li>
+                <li><a href="WorkingWithUs.php">Working  With Us</a></li>
+           </ul>
+        </li>
+        
+	<li style="position:absolute;left:1050px;top:450px" ><a href="OurServices.php" id="n2">Services</a> 
+       	  <ul style="display:none" class="sub_menu">
+                <li><a href="OperationalExcellence.php">Operational  Excellence</a></li>
+                <li><a href="Business.php">Business  Process Management</a></li>
+                <li><a href="Strategy.php">Strategy  &amp; Growth</a></li>
+				<li><a href="operational-architecture.php">Operational  Architecture</a></li>
+                <li><a href="Performance.php">Performance  Management</a></li>
+                <li><a href="Diagnostics.php">Diagnostics  &amp; Analytics</a></li>
+                <li><a href="Training.php">Training Services</a></li>
+           </ul>
+        
+	</li>
+        
+     	</ul>
+   </div>
+         	
+  </div>
+
+   <div class="content">
+   		<div class="leftarea"  style="position:absolute;left:217px;top:550px;width:100px">
+			<div class="leftnav"> 
+			<a href="OurApproach.php">Our Approach</a>
+			
+			<p>&nbsp</p>
+
+			<a href="OurServices.php">Our Services</a>
+			
+			<p>&nbsp</p>
+
+			<a href="OurIndustries.php">Our Industries</a>
+
+			
+			<p>&nbsp</p>
+				
+			<a href="OurClients.php">Our Exposure</a>
+
+			<p>&nbsp</p>
+
+			<a href="ContactUs.php">Contact Us</a>
+</div>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 
+    
+</div>
+<hr style="position:absolute;left:350px;top:525px;width:0px;height:105%;"/>
+<hr style="position:absolute;right:410px;top:525px;width:0px;height:105%;"/>
+<hr style="position:absolute;left:190px;top:590px;width:150px;height:0px;"/>
+<hr style="position:absolute;left:190px;top:590px;width:150px;height:0px;"/>
+<hr style="position:absolute;left:190px;top:655px;width:150px;height:0px;"/>
+<hr style="position:absolute;left:190px;top:720px;width:150px;height:0px;"/>
+<hr style="position:absolute;left:190px;top:785px;width:150px;height:0px;"/>
+<hr style="position:absolute;left:190px;top:850px;width:150px;height:0px;"/>
+
+         <div class="centerarea"  style="position:absolute;left:360px;top:525px;right:420px;font-size:100%;text-align:justify;">
+
+<p> BigM is a Business Consulting and Leadership Training Service Provider with vast expertise in Business Process Excellence, Six Sigma, Strategy Management ,Operation and Supply Chain Transformation  in Manufacturing Domain.
+
+</p>
+          
+<p>  We are a team of professionals from IITs and IIMs, with extensive and diverse experience in Automotive, Steel, Heavy Industries, Tools and Die  Making, Defense, Pharmaceuticals, Human Resource and Training sectors.
+
+</p>
+<p>Our expertise lies in Designing, Customising and Delivering Consulting and Training Engagements as per customers requirements.
+</p>
+
+<p>&nbsp;</p>
+
+        
+     </div>
+
+<div class="rightarea" style="position:absolute;left:370px;top:750px;align-items:center;">
+
+<img src="images/AboutUs2.jpg" width="550" height="350" alt="home" style="position:relative;" />
+	</div>
+     <div class="clear"></div>
+
+
+</div>
+
+
+        	<div class="clear"></div>
+  
+   <div class="footer"  style="position:absolute;left:500px;top:1150px;">
+
+   	<div class="fleft"  >
+<a href="index.php">Home</a> |   <a href="privacy.php">Privacy</a>  |  
+<a href="legal.php">Legal</a>  |  <a href="contact-us.php">Contact</a>
+</div>
+      
+        	<div class="clear"></div>
+  </div>
+
+
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12669334-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
+	document.getElementById("n1").className = "active";
+</script>
+
+</body>
+
+</html>
