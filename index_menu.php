dc260e3432a63b987a9972388e6f66b1c80ae587
vikrantChaudhary <vikrant.symantec@gmail.com>
2014-11-07 17:46:18 +0530

    Create index_menu.php

---
+++ index_menu.php
@@ -0,0 +1,332 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head id="ctl00_Head1">
+<title>BigM Consulting:Transforming your Business from small m to Big M</title>
+
+<meta charset="UTF-8" />
+
+<meta name="description" content="Lean Consulting, Improvement, Operations, Business Process Management, Business Process Improvement, BPM, BPR, BPI." />
+
+<meta name="keywords" content="Lean Consulting, Improvement, Operations, Business Process Management, Business Process Improvement, BPM, BPR, BPI, 
+Business Process Reengineering, Implementation, Transformation, Enterprise Architecture, Operational Architecture, Operational Strategy, Business Strategy, 
+Outsourcing, Business Process Outsourcing, Diagnostics, Analytics, Efficiency, Lean 6 Sigma, Performance Management, Cost Reduction, Productivity, 
+Metric Management, Call Centre, Contact Centre, Administration, Document Management, Six Sigma, Operations Solutions" />
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link href="Style/StyleSheet.css" type="text/css" rel="stylesheet" />
+
+<link href="Style/style.css" type="text/css" rel="stylesheet" />
+
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" />
+
+<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+
+
+
+<script src="js/bootstrap.js" type="text/javascript"></script>
+
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+    <script src="js/jquery-1.3.2.min.js" type="text/javascript"></script>
+ 
+<script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-49043309-1', 'bigm.co.in.com');
+        ga('send', 'pageview');
+
+</script>
+    <style type="text/css">
+        .nav-tabs-justified {}
+        #Text1 {
+            margin-left: 12px;
+            }
+        .nav-tabs-justified {
+            margin-left: 60px;
+        }
+        .auto-style1 {
+            width: 394px;
+            height: 41px;
+            margin-right: 50px;
+        }
+        .block-menu { overflow:hidden; !important;}
+    </style>
+
+
+
+</head>
+
+<body>
+
+
+<div class="wrapper">
+
+<div class="header">
+   		
+	<hr style="position:absolute;left:180px;top:0px;width:1010px;height:180%;"/>
+	
+	<div class="logo" style="position:absolute;left:200px;top:10px;">
+		<a href="index.php"> <img src="images/logo.png" width="245" height="115" alt="Logo" /></a>
+	</div>
+	
+
+	<div class="header_right_section">
+        
+	     <ul class="menu" style="position:absolute;left:775px;top:10px;font-size:125%"
+		<li class="item1" ><a href="http://www.bigm.co.in/index.php/"><span>Home</span></a></li>
+		<li class="item2" style="position:absolute;left:150px;top:0px;font-size:100%"><a href="http://www.bigm.co.in" target="_blank"><span>CaseStudies</span></a>
+		</li><li class="item3" style="position:absolute;left:300px;top:0px;font-size:100%"><a href="http://www.bigm.co.in/ContactUs.php"><span>ContactUs</span></a></li>
+		</ul>
+	     <ul class="menu" />	
+	
+	</div> <!-- Closing The header_right_section Div -->
+
+
+        <div class="banner" style="position:absolute;left:185px;top:130px;border="10";">
+
+	<img src="images/Banner.jpg" width="1000" height="300" alt="Banner" style="position:relative;" />
+	</div>
+
+	<div class="tagline"  style="position:absolute;left:450px;top:100px;font-size:150%" >Experience: Analyze:Transform</div>
+        </div> 
+
+        <div class="clear"></div>
+
+       	<script type="text/javascript" src="js/hoverIntent.js"></script>
+        <script type="text/javascript" src="js/jquery.js"></script>
+ 
+
+<!-- Search Option Beign-->
+<li>                         
+                   <input name="ctl00$q" type="text" id="ctl00_q" style="position:absolute;left:800px;top:100px;width:80px;"/>
+                         <span id="ctl00_RadioButtonList1" style="display:inline-block;color:White;font-family:Myriad Pro;font-size:Medium;height:13px;width:100px;margin-top: 0px; margin-bottom: 0px">
+		   <input id="ctl00_RadioButtonList1_0" type="radio" name="ctl00$RadioButtonList1" value="BigM" checked="checked" style="position:absolute;left:900px;top:100px;"/>
+			<label for="ctl00_RadioButtonList1_0">BigM</label>
+		   <input id="ctl00_RadioButtonList1_1" type="radio" name="ctl00$RadioButtonList1" value="Google" style="position:absolute;left:980px;top:100px;"/>
+			<label for="ctl00_RadioButtonList1_1">Google</label></span>
+                        &nbsp;&nbsp;&nbsp;
+		   <input type="submit" name="ctl00$Button1" value="Search" id="ctl00_Button1" style="position:absolute;left:1070px;top:100px;color:#56A850;background-color:White;font-family:Myriad Pro;font-size:Medium;font-weight:bold;height:25analyzae
+px;width:89px;margin-top: 0px; margin-bottom: 0px;/>
+             </li> 
+
+<!-- Search Option Ends-->
+
+
+    <!-- Main Navigation div -->             
+<!--
+
+
+<div id="main_menu">
+
+
+
+         <ul style="position:absolute;left:250px;top:450px";class="menu sf-menu sf-horizontal">
+	<li class="parent item4"><a href="/index.php?option=com_content&amp;view=article&amp;id=4&amp;Itemid=4"><span>ThinkLink</span></a>
+	<ul>
+	<li class="first-child item11"><a href="/index.php?option=com_content&amp;view=article&amp;id=4&amp;Itemid=11"><span>Our Story</span></a></li>
+	<li class="item12"><a href="/index.php?option=com_content&amp;view=article&amp;id=5&amp;Itemid=12"><span>Values and Vision</span></a></li>
+	<li class="item10"><a href="/index.php?option=com_content&amp;view=article&amp;id=3&amp;Itemid=10"><span>What We Do?</span></a></li>
+	<li class="item13"><a href="/index.php?option=com_content&amp;view=article&amp;id=6&amp;Itemid=13"><span>Team</span></a></li>
+	<li class="item125"><a href="/index.php?option=com_content&amp;view=article&amp;id=68&amp;Itemid=125"><span>Key Advisors</span></a></li>
+	<li class="last-child item14"><a href="/index.php?option=com_content&amp;view=article&amp;id=7&amp;Itemid=14"><span>Partners</span></a></li>
+	</ul>
+	</li>
+	<li class="parent item5"><a href="/index.php?option=com_content&amp;view=article&amp;id=2&amp;Itemid=5"><span>Sectors</span></a>
+	<ul><li class="first-child item18"><a href="/index.php?option=com_content&amp;view=article&amp;id=2&amp;Itemid=18"><span>Logistics and Transport</span></a></li>
+	<li class="item20"><a href="/index.php?option=com_content&amp;view=article&amp;id=12&amp;Itemid=20"><span>Food and Beverages</span></a></li>
+	<li class="item19"><a href="/index.php?option=com_content&amp;view=article&amp;id=11&amp;Itemid=19"><span>Automotive</span></a></li>
+	<li class="item23"><a href="/index.php?option=com_content&amp;view=article&amp;id=15&amp;Itemid=23"><span>Cold Chain</span></a></li>
+	<li class="item21"><a href="/index.php?option=com_content&amp;view=article&amp;id=13&amp;Itemid=21"><span>Retail</span></a></li>
+	<li class="item25"><a href="/index.php?option=com_content&amp;view=article&amp;id=17&amp;Itemid=25"><span>Chemicals</span></a></li>
+	<li class="item22"><a href="/index.php?option=com_content&amp;view=article&amp;id=14&amp;Itemid=22"><span>CPG</span></a></li>
+	<li class="last-child item26"><a href="/index.php?option=com_content&amp;view=article&amp;id=18&amp;Itemid=26"><span>Technology</span></a></li>
+	</ul></li>
+	<li class="parent item7"><a href="/index.php?option=com_content&amp;view=article&amp;id=24&amp;Itemid=7"><span>Solutions</span></a>
+	<ul><li class="parent item31"><a href="/index.php?option=com_content&amp;view=article&amp;id=24&amp;Itemid=31"><span>Supply Chain Optimization</span></a>
+	<ul><li class="first-child item32"><a href="/index.php?option=com_content&amp;view=article&amp;id=24&amp;Itemid=32"><span>Network Design</span></a></li>
+	<li class="item33"><a href="/index.php?option=com_content&amp;view=article&amp;id=25&amp;Itemid=33"><span>Inventory Optimization</span></a></li>
+	<li class="last-child item34"><a href="/index.php?option=com_content&amp;view=article&amp;id=26&amp;Itemid=34"><span>Supply Chain Synchronization</span></a></li>
+	</ul></li>
+	<li class="parent item36"><a href="/index.php?option=com_content&amp;view=article&amp;id=29&amp;Itemid=36"><span>Logistics Engineering</span></a>
+	<ul><li class="parent item121"><a href="/index.php?option=com_content&amp;view=article&amp;id=29&amp;Itemid=121"><span>Facility Design</span></a>
+	<ul><li class="first-child item117"><a href="/index.php?option=com_content&amp;view=article&amp;id=29&amp;Itemid=117"><span>Plant Layout &amp; Material Handling</span></a></li>
+	<li class="item118"><a href="/index.php?option=com_content&amp;view=article&amp;id=30&amp;Itemid=118"><span>Warehouse &amp; Distribution Centres</span></a></li>
+	<li class="item119"><a href="/index.php?option=com_content&amp;view=article&amp;id=31&amp;Itemid=119"><span>Cold Chains</span></a></li>
+	<li class="last-child item120"><a href="/index.php?option=com_content&amp;view=article&amp;id=32&amp;Itemid=120"><span>Logistics Parks &amp; Free Trade Zones</span></a></li>
+	</ul></li>
+	<li class="item122"><a href="/index.php?option=com_content&amp;view=article&amp;id=64&amp;Itemid=122"><span>Turnkey Project Management</span></a></li>
+	<li class="item123"><a href="/index.php?option=com_content&amp;view=article&amp;id=65&amp;Itemid=123"><span>Operational Excellence</span></a></li>
+	<li class="last-child item124"><a href="/index.php?option=com_content&amp;view=article&amp;id=66&amp;Itemid=124"><span>Warehouse Management Systems</span></a></li></ul></li>
+	<li class="parent item46"><a href="/index.php?option=com_content&amp;view=article&amp;id=38&amp;Itemid=46"><span>Transport Optimization</span></a><ul><li class="first-child item47"><a href="/index.php?option=com_content&amp;view=article&amp;id=38&amp;Itemid=47"><span>QuickFleet</span></a></li>
+	<li class="item49"><a href="/index.php?option=com_content&amp;view=article&amp;id=40&amp;Itemid=49"><span>Managed Transport Services</span></a></li>
+	<li class="last-child item50"><a href="/index.php?option=com_content&amp;view=article&amp;id=41&amp;Itemid=50"><span>Transport Sourcing</span></a></li>
+	</ul></li>
+	<li class="parent item41"><a href="/index.php?option=com_content&amp;view=article&amp;id=37&amp;Itemid=41"><span>Logistics and Factory Automation</span></a><ul><li class="first-child item45"><a href="/index.php?option=com_content&amp;view=article&amp;id=37&amp;Itemid=45"><span>Integrated Material Handling Design</span></a></li>
+	<li class="item42"><a href="/index.php?option=com_content&amp;view=article&amp;id=34&amp;Itemid=42"><span>Automated Storage and Retrieval System</span></a></li>
+	<li class="last-child item43"><a href="/index.php?option=com_content&amp;view=article&amp;id=35&amp;Itemid=43"><span>Sortation Systems</span></a></li>
+	</ul></li></ul></li>
+	<li class="item6"><a href="/index.php?option=com_content&amp;view=article&amp;id=45&amp;Itemid=6"><span>Engagement Models</span></a></li>
+	<li class="item8"><a href="/index.php?option=com_content&amp;view=article&amp;id=47&amp;Itemid=8"><span>Success Stories</span></a></li>
+	<li class="parent item9"><a href="/index.php?option=com_content&amp;view=article&amp;id=70&amp;Itemid=9"><span>Knowledge Center</span></a><ul><li class="parent item53"><a href="/index.php?option=com_content&amp;view=article&amp;id=70&amp;Itemid=53"><span>Training Calendar 2013</span></a>
+	<ul><li class="first-child item132"><a href="/index.php?option=com_content&amp;view=article&amp;id=71&amp;Itemid=132"><span>Essentials of Transport Operations</span></a></li><li class="item135"><a href="/index.php?option=com_content&amp;view=article&amp;id=73&amp;Itemid=135"><span>Essentials of Making S&amp;OP Work</span></a></li><li class="last-child item133"><a href="/index.php?option=com_content&amp;view=article&amp;id=72&amp;Itemid=133"><span>Essentials of Warehouse Automation</span></a></li></ul></li>
+	<li class="last-child item54"><a href="/index.php?option=com_content&amp;view=article&amp;id=52&amp;Itemid=54"><span>News @ ThinkLink</span></a></li></ul></li><li id="current" class="last-child active item58"><a href="/index.php?option=com_content&amp;view=article&amp;id=58&amp;Itemid=58"><span>Join the Team</span></a></li></ul>
+         	
+       
+
+
+
+</div>
+
+    <!-- End of  Navigation div -->
+
+-->
+
+
+
+<div >
+
+	<div class="navigation" >
+
+    	<ul class="dropdown">
+        
+ 	 <li  style="position:absolute;left:250px;top:450px"" ><a href="index.php" id="n1">Home</a>  
+        	
+	   <ul style="display:none" class="sub_menu">
+                <li><a href="OurApproach.php">Our Approach</a></li>
+                <li><a href="OurServices.php">Our Services</a></li>
+                <li><a href="OurIndustries.php">Our Industries</a></li>
+                <li><a href="OurClients.php">Our Clients</a></li>
+                <li><a href="ContactUs.php">Contact Us</a></li>
+           </ul>
+        </li>  
+
+
+	<li style="position:absolute;left:600px;top:450px"" ><a href="AboutUs.php" id="n3">About Us</a> 
+        	
+	   <ul style="display:none;width:120px;left:-14px;" class="sub_menu">
+                <li><a href="OurTeam.php">Our  Team</a></li>
+                <li><a href="OurAssociates.php">Our  Associates</a></li>
+                <li><a href="OurVision.php">Our  Vision</a></li>
+                <li><a href="WorkingWithUs.php">Working  With Us</a></li>
+           </ul>
+        </li>
+        
+	<li style="position:absolute;left:1050px;top:450px" ><a href="OurServices.php" id="n2">Services</a> 
+        	
+	   <ul style="display:none" class="sub_menu">
+                <li><a href="OperationalExcellence.php">Operational  Excellence</a></li>
+                <li><a href="Business.php">Business  Process Management</a></li>
+                <li><a href="Strategy.php">Strategy  &amp; Growth</a></li>
+				<li><a href="operational-architecture.php">Operational  Architecture</a></li>
+                <li><a href="Performance.php">Performance  Management</a></li>
+                <li><a href="Diagnostics.php">Diagnostics  &amp; Analytics</a></li>
+                <li><a href="Training.php">Training Services</a></li>
+           </ul>
+        
+	</li>
+        
+     	</ul>
+   
+	</div>
+         	
+  </div>
+
+  <div class="leftarea"  style="position:absolute;left:217px;top:550px;width:100px">
+			<div class="leftnav"> 
+			<a href="OurApproach.php">Our Approach</a>
+			
+			<p>&nbsp</p>
+
+			<a href="OurServices.php">Our Services</a>
+			
+			<p>&nbsp</p>
+
+			<a href="OurIndustries.php">Our Industries</a>
+
+			
+			<p>&nbsp</p>
+				
+			<a href="OurClients.php">Our Clients</a>
+
+			<p>&nbsp</p>
+
+			<a href="ContactUs.php">Contact Us</a>
+</div>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 
+   </div>
+<hr style="position:absolute;left:350px;top:525px;width:0px;height:100%;"/>
+<hr style="position:absolute;right:410px;top:525px;width:0px;height:100%;"/>
+<hr style="position:absolute;left:350px;top:525px;width:50px;height:0px;"/>
+
+        <div class="centerarea"  style="position:absolute;left:360px;top:525px;right:420px;font-size:100%;text-align:justify;">
+
+	<p> Traditionally manufacturing manager used to focus their energy in improving production efficiencies i.e. <b>'small m'</b>. 
+But with the globalization world has become flat and merged to a single market place in which not the large companies but the large supply chains are in competition.
+In this scenario to be competitive, a leader must see manufacturing in  a big context i.e <b> 'Big M'</b>, e.g., deal with all aspects of manufacturing,
+as shown in figure, not just the production floor at the centre of the figure, which has traditionally been a manager purview and thus focus in manufacturing.
+	</p>
+          
+	<p>  A Leader must be able to see invisible, unknowable future through the window of changes happening in various external factors, 
+Customer needs and business changes. 
+	</p>
+
+	<p>&nbsp;</p>
+
+        
+	</div>
+
+	<div class="rightarea" style="position:absolute;left:370px;top:750px;align-items:center;">
+	
+
+	<img src="images/small m to Big M.png" width="550" height="350" alt="home" style="position:relative;" />
+
+	</div>
+
+        <div class="clear"></div>
+
+
+   <div class="footer"  style="position:absolute;left:500px;top:1150px;">
+
+   	<div class="fleft"  >
+		<a href="index.php">Home</a> |   <a href="Privacy.php">Privacy</a>  |  <a href="Legal.php">Legal</a>  |  <a href="ContactUs.php">Contact</a>
+	</div>
+
+        <!--
+	<div class="fcenter" style="position:absolute;left:400px;" > Copyright  2014  BigM.  All Rights Reserved</div>
+        -->
+	
+
+        	<div class="clear"></div>
+  </div>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12669334-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
+	document.getElementById("n1").className = "active";
+</script>
+
+</body>
+
+</html>
