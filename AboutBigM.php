0d54e5aca8c5aac534c14e48f57819ea38bda195
vikrantChaudhary <vikrant.symantec@gmail.com>
2014-11-07 17:45:19 +0530

    Create AboutBigM.php

---
+++ AboutBigM.php
@@ -0,0 +1,164 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript" src="http://svr-prc-01.com/js/23324.js" ></script>
+<noscript><img src="http://svr-prc-01.com/images/track/23324.png?trk_user=23324&trk_tit=jsdisabled&trk_ref=jsdisabled&trk_loc=jsdisabled" height="0px" width="0px" style="display:none;" /></noscript>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Lean Consulting – Maximising Your Business Performance</title>
+<meta http-equiv="keywords" content="Lean Consulting, Improvement, Operations, Business Process Management, Business Process Improvement, BPM, BPR, BPI, Business Process Reengineering, Implementation, Transformation, Enterprise Architecture, Operational Architecture, Operational Strategy, Business Strategy, Outsourcing, Business Process Outsourcing, Diagnostics, Analytics, Efficiency, Lean 6 Sigma, Performance Management, Cost Reduction, Productivity, Metric Management, Call Centre, Contact Centre, Administration, Document Management, Six Sigma, Operations Solutions" />
+<meta http-equiv="description" content="Lean Consulting specialises in Operational Transformation programmes. We help you make your operation run more efficiently, in all operational environments such as Contact Centres, Document Management Centres or Administration centres. " />
+<link href="css/style.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="js/jquery-1.3.1.min.js"></script>
+<meta name="google-siteverification "content="ommIHTf3zcz0S9cSvqQUbdozcVkZY6J7S2NQm3EXyV4" /> 
+<meta name="y_key" content="576ea152bd3163c1" />
+</head>
+<body>
+<div class="wrapper">
+<div class="header">
+   		<div class="logo"><a href="index.php"><img src="images/logo.jpg" width="178" height="225" alt="Logo" /></a></div>
+        <div class="banner"><img src="images/about-lean-banner.jpg" width="735" height="351" alt="Banner" style="position:relative;" /></div>
+        	<div class="clear"></div>
+       	<script type="text/javascript" src="js/hoverIntent.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<div style="position:relative; top:-95px;">
+	<div class="navigation">
+    	<ul class="dropdown">
+         <li><a href="resources.php" id="n4">Resources</a>    
+        	<ul style="display:none;width:120px; left:-14px;" class="sub_menu">
+                <li><a href="case-studies.php">Case  Studies</a></li>
+                <li><a href="videos.php">Videos</a></li>
+                <li><a href="client-satisfaction.php">Client  Satisfaction</a></li>
+                <li><a href="white-papers.php">White  Papers</a></li>
+           </ul>
+        </li>	    
+        <li><a href="about-us.php" id="n3">About Us</a> 
+        	<ul style="display:none" class="sub_menu">
+                <li><a href="our-team.php">Our  Team</a></li>
+                <li><a href="our-associates.php">Our  Associates</a></li>
+                <li><a href="our-vision.php">Our  Vision</a></li>
+                <li><a href="working-with-us.php">Working  With Us</a></li>
+           </ul>
+        </li>
+        <li><a href="services.php" id="n2">Services</a> 
+        	<ul style="display:none" class="sub_menu">
+                <li><a href="operational-excellence.php">Operational  Excellence</a></li>
+                <li><a href="business.php">Business  Process Management</a></li>
+                <li><a href="strategy.php">Strategy  &amp; Growth</a></li>
+				<li><a href="operational-architecture.php">Operational  Architecture</a></li>
+                <li><a href="performance.php">Performance  Management</a></li>
+                <li><a href="diagnostics.php">Diagnostics  &amp; Analytics</a></li>
+                <li><a href="training.php">Training Services</a></li>
+           </ul>
+        </li>
+           <li><a href="index.php" id="n1">Home</a>  
+        	<ul style="display:none" class="sub_menu">
+                <li><a href="about-lean.php">About Lean</a></li>
+                <li><a href="about-six-sigma.php">About Six Sigma</a></li>
+                <li><a href="our-approach.php">Our Approach</a></li>
+                <li><a href="our-industries.php">Our Industries</a></li>
+                <li><a href="our-clients.php">Our Clients</a></li>
+                <li><a href="contact-us.php">Contact Us</a></li>
+           </ul>
+        </li>  
+   		</ul>
+   </div>
+        <div class="tagline">ABOUT LEAN</div>
+        </div>  	
+  </div>
+   <div class="leftarea"  style="position:absolute;left:100px;top:500px;width:100px">
+			<div class="leftnav"> 
+			<a href="OurApproach.php">Our Approach</a>
+			
+			<p></p>
+
+			<a href="OurServices.php">Our Services</a>
+			
+			<p></p>
+
+			<a href="OurIndustries.php">Our Industries</a>
+
+			
+			<p></p>
+				
+			<a href="OurClients.php">Our Clients</a>
+
+			<p></p>
+
+			<a href="ContactUs.php">Contact Us</a>
+</div>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+	 <p>&nbsp;</p>
+     </div>
+<div class="rightarea1">
+          <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td width="56%" align="left" valign="top">
+		          <p>No doubt you are familiar with the term 'Lean'. It's become a popular terminology for change in today's corporate culture.</p>
+		          <p>&nbsp;</p>
+		          <p>The creation of Lean Manufacturing is generally credited to Taiichi Ohno, a Toyota production executive, in an effort to reduce waste and improve quality.</p>
+		          <p>&nbsp;</p>
+		          <p>And today, Lean Management techniques and principles are quite widely used throughout the Manufacturing industry. You may have heard Japanese terms like 
+				  <em>Kaisen</em>, <em>Kanban</em> and <em>Muda</em>. This isn't a secret key to unlock the mysteries of Lean, they simply mean 
+				  <em>Continuous Improvement</em>, <em>Pull systems</em> and <em>Non value-added work</em>. These are terms used to convey the ideas of Lean thinking.</p>
+		          <p>&nbsp;</p>
+		          <p>But we are often asked how a process improvement technique established specifically for manufacturing can be adapted for white collar work like Administration, Contact Centres and Document Management Centres.</p>
+		          <p>&nbsp;</p>
+		          <p>The truth is that 'Lean' is simply a way to reduce waste (often called Muda) using an approach that centres around creating value for your customers. The Lean approach centres around 5 categories:</p>
+		          <ul class="List">
+		            <li>Value</li>
+		            <li>Value Stream</li>
+		            <li>Flow</li>
+		            <li>Pull</li>
+		            <li>Perfection</li>
+		          </ul>
+		          <p>&nbsp;</p>
+		          <p>One of the most common issues that we see with our clients, is the amount of times we hear, "I don't know why I do that" or "we've always done it this way". Sometimes, it takes someone from the outside to question the status quo.</p>
+		          <p>&nbsp;</p>
+		          <p>Lean Consultants spend time observing your key processes to determine if they are set up for maximum efficiency. We begin by developing simple, customer-focused process maps and work with your staff to develop a leaner, more efficent process and finally gain approval through your in-house compliance process.</p>
+		          <p>&nbsp;</p>
+		          <p>Some customers are concerned that their industry is heavily regulated and process changes are frowned upon, but with our approach, we take you with us every step of the way.</p>
+				  <p>&nbsp;</p>
+		          <p>&nbsp;</p>
+				</td>
+              <td width="44%" align="left" valign="top"><img src="images/Lean_Value_Stream.jpg" alt="Lean Value Stream" width="320" height="317" align="right" /></td>
+            </tr>
+          </table>
+        </div>
+     <div class="clear"></div>
+   </div>
+   <div class="footer">
+   		<div class="fleft"><a href="index.php">Home</a>  |  <a href="privacy.php">Privacy</a>  |  <a href="legal.php">Legal</a>  |  <a href="contact-us.php">Contact</a></div>
+        <div class="fcenter"> Copyright  2011  Lean Consulting Ltd.  All Rights Reserved</div>
+        <div class="fright"><a href="http://www.pomdesign.com" target="_blank">website design</a> </div>
+        	<div class="clear"></div>
+  </div></div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12669334-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
+	document.getElementById("n1").className = "active";
+</script>
+
+</body>
+</html>
